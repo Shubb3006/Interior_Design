c1552ad8aaecceccdcb06047e6f4a4d468fbb307
Shubb3006 <sm325642@gmail.com>
2026-01-17 20:12:29 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173",
+      "https://interior-design-ctac.onrender.com/"],
       credentials: true,
     })
   ); 
