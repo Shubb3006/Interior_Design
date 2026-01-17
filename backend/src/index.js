f6282b2da576c2715ee485132bcc94e75cb50ff5
Shubb3006 <sm325642@gmail.com>
2026-01-17 20:14:00 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import adminRoutes from "./routes/admin.route.js"
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
+import path from "path";
 import cors from 'cors';
 
 
@@ -23,9 +24,22 @@ app.use(
     })
   ); 
 
+  const __dirname = path.resolve();
+
 app.use("/api/leads",leadRoutes)
 app.use("/api/admin",adminRoutes)
 app.use("/api/auth",authRoutes)
+
+
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.join(__dirname, "../frontend/dist");
+
+  app.use(express.static(distPath));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
 
 app.listen(PORT,()=>{
     connectDB();
